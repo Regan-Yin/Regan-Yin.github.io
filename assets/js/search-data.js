713b2b162b6ea7433f330e482124bf9fb687218c
Regan-Yin <97799012+Regan-Yin@users.noreply.github.com>
2025-07-02 23:27:09 +0000

    Deploying to gh-pages from @ Regan-Yin/Regan-Yin.github.io@6afe86f203e4afc5b65907e6a6f02c314adbab6c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -107,35 +107,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%65%67%61%6E%63%68%79@%73%74%75%64%65%6E%74.%75%62%63.%63%61", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Regan-Yin", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/regan-ch-yin", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-kaggle',
+        title: 'Kaggle',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.kaggle.com/reganyin", "_blank");
         },
       },{
       id: 'light-theme',
