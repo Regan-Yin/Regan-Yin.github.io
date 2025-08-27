cf17a336a5daa7512092a0d67e14b1bdade4a488
Regan-Yin <97799012+Regan-Yin@users.noreply.github.com>
2025-08-27 06:36:54 +0000

    Deploying to gh-pages from @ Regan-Yin/Regan-Yin.github.io@5cb586477280e2db01baa4ec9e6f1aca8372bb64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -67,9 +67,9 @@ ninja.data = [{
           description: "US Election/Data Analytics/Data Visualisation/Web Scrapping/Web App with Dash/Beautiful Soup/Selenium",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-predictive-models-for-heart-disease-diagnosis",
+          title: 'Predictive Models for Heart Disease Diagnosis',
+          description: "Health Care/Data Mining/Logistic Regression/Data Visualization/Machine Learning/",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
